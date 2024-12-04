0837f20f860b0aed6942aab127b3d75099741146
Atlas-418 <twobraincellbeing@gmail.com>
2024-12-03 17:37:15 -0700

    actually did some things, figuring out how to build a mesh out of vectors.

--- src/camera.rs
+++ src/camera.rs
@@ -6,6 +6,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, start_camera);
+        app.add_systems(Update, (cam_move, cam_look));
     }
 }
 
@@ -15,15 +16,25 @@ struct CameraController {
     turn_speed: f32,
 }
 
-
 #[derive(Debug, Component)]
 struct Player;
 
-
-#[derive(Debug, Component)]
-struct WorldModelCamera;
-
 fn start_camera(
+    mut cmd: Commands,
+){
+    cmd.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 5.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+fn cam_move(
+
+){
+
+}
+
+fn cam_look(
 
 ){
 
--- src/world.rs
+++ src/world.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 pub struct GenWorldPlugin;
 impl Plugin for GenWorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, gen_world);
+        app.add_systems(Startup, add_world);
     }
 }
 
@@ -16,12 +16,31 @@ struct Voxel {
     active: bool,
 }
 
-fn gen_world(
+fn add_world(
     mut cmd: Commands,
 ) {
-
+    let maze = make_maze();
+    let world = gen_world(maze);
+    
+    cmd.spawn((
+        Mesh3d(world),
+        //MeshMaterial3d(),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
 }
 
-fn make_maze() {
-    
+fn gen_world(
+    points: Vec2,
+) -> Mesh {
+    Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default())
+        .with_inserted_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            vec![[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [1.0, 1.0, 0.0], [1.0, 0.0, 1.0], [0.0, 1.0, 1.0], [1.0, 1.0, 1.0]]
+        )
+}
+
+fn make_maze() -> Vec2 {
+    let mut maze = Vec2::new(MAZE_HEIGHT.into(), MAZE_WIDTH.into())
+
+    maze
 }
